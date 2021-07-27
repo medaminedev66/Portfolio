8bad6d851729db7909f39451efd0acace1f6c954
rachid <rachidboudaoudi95@gmail.com>
2021-07-27 18:33:46 +0100

    Add linter fiule for js

--- menu.js
+++ menu.js
@@ -1,33 +1,23 @@
+const closeMenu = () => {
+    document.querySelector('header').classList.remove('mobile-menu');
+    document.querySelector('nav > img').classList.remove('visible');
+    document.querySelector('nav > ul').classList.remove('mobile-nav');
+    document.querySelector('nav').classList.remove('nav-strech');
+    document.querySelector('header > div').classList.remove('visible');
+
+}
+
 const openMenu = () => {
-    document.querySelector('header').classList.toggle('mobile-menu');
-    document.querySelector('nav > img').classList.toggle('visible');
-    document.querySelector('nav > ul').classList.toggle('mobile-nav');
-    document.querySelector('nav').classList.toggle('nav-strech');
-    document.querySelector('header > div').classList.toggle('visible');
+    document.querySelector('header').classList.add('mobile-menu');
+    document.querySelector('nav > img').classList.add('visible');
+    document.querySelector('nav > ul i').classList.toggle('visible');
+    document.querySelector('nav > ul').classList.add('mobile-nav');
+    document.querySelector('nav').classList.add('nav-strech');
+    document.querySelector('header > div').classList.add('visible');
 }
 document.querySelector('nav>img').addEventListener('click', openMenu);
 const items = Array.from(document.querySelectorAll('ul > li a'));
 for (let i = 0; i < items.length; i += 1) {
-    items[i].addEventListener('click', openMenu);
+    items[i].addEventListener('click', closeMenu);
 }
-document.querySelector('ul > li i').addEventListener('click', openMenu);
-
-
-
-// const menuAction = () => {
-
-// }
-
-// function closeMenu() {
-//     const menu = document.querySelector('nav>ul');
-//     const menuItem = Array.from(document.querySelectorAll('nav > ul li'));
-//     for (let item = 0; item < menuItem.length; item += 1) {
-//         menuItem[item].classList.remove('menu-item');
-//     }
-//     menu.classList.remove('mobile-menu');
-//     document.querySelector('nav > img').classList.toggle('visible');
-//     document.querySelector('nav > div').classList.toggle('visible');
-// }
-
-
-// const closeBtn = document.querySelector('.closing-btn');
+document.querySelector('ul > li i').addEventListener('click', closeMenu);
