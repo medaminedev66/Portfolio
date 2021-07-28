effab235618556d8d24b9e261b0d7205d5c98573
medaminedev66 <md.amine.smahi@gmail.com>
2021-07-28 14:29:29 +0100

    Add classes to the new elements of pop up container and style them

--- popup.js
+++ popup.js
@@ -65,6 +65,18 @@ container.appendChild(btnClose);
 container.appendChild(technologies);
 container.appendChild(infoDiv);
 
+// Adding classes
+
+container.className = 'popContainer';
+title.className = 'popTitle';
+description.className = 'popDescr';
+image.className = 'popImage';
+technologies.className = 'popTec';
+btnOne.className = 'popBtn';
+btnTwo.className = 'popBtn';
+
+
+
 function pop(index){
   let techHtml = '';
   document.body.appendChild(container);
@@ -117,6 +129,19 @@ document.querySelector('.six').addEventListener('click',() => {
 
 // closing event
 
+
+// title.innerText="The first title"
+// btnOne.innerText="See Demo"
+// image.src='./img/placeholder.png'
+// description.innerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+// const openPopUp = () => {
+//   console.log('clicked')
+//   document.body.appendChild(container);
+// //   document.querySelector('body').classList.add('stop-scrol')
+
+// }
+
+// document.querySelector('.btn-project').addEventListener('click', openPopUp);
 
 
 
