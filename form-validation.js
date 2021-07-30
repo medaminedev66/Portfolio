5c24234c4be5e974548de6ca5a881fb6b082448f
rachid <rachidboudaoudi95@gmail.com>
2021-07-30 13:32:28 +0100

    Fix linter errors & remove unused code in js

--- form-validation.js
+++ form-validation.js
@@ -1,34 +1,23 @@
 function checkvalidation(sentencef) {
-   let size = sentencef.length
-   var lowercase = sentencef.toLowerCase()
-   if(lowercase===sentencef){
-        return false  }
-    else{
-          return true
-    }
-} 
-
-function validateEmail(email,input){
-    const msg = input.parentNode.querySelector("small");
-    if (checkvalidation(email))
-    {
-	     msg.innerText = "The email should be in lower case";
-	     input.className = "error";
-	     return false;
-    }
-    else{
-        return true;
-    }
-
+  const lowercase = sentencef.toLowerCase();
+  if (lowercase === sentencef) return false;
+  return true;
 }
 
-const form = document.querySelector("#contactme")
+function validateEmail(email, input) {
+  const msg = input.parentNode.querySelector('small');
+  if (checkvalidation(email)) {
+    msg.innerText = 'The email should be in lower case';
+    input.className = 'error';
+    return false;
+  }
+  return true;
+}
 
-
-form.addEventListener('submit', function(event){
-    console.log("clicked")
-    let email = form.elements['email'].value
-    if (!validateEmail(email, form.elements['email'])) {
-        event.preventDefault();
-    }   
-})
+const form = document.querySelector('#contactme');
+form.addEventListener('submit', (event) => {
+  const email = form.elements.email.value;
+  if (!validateEmail(email, form.elements.email)) {
+    event.preventDefault();
+  }
+});
