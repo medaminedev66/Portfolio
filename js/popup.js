8da081e451f134860e52b8c806b32aff9a2491b2
Amine Smahi <md.amine.smahi@gmail.com>
2022-02-12 11:57:41 +0100

    Add some mobile screenshots

--- js/popup.js
+++ js/popup.js
@@ -31,7 +31,7 @@ const projects = [
   {
     title: 'Countries of all the World',
     technologies: ['Css', 'Bootstrap', 'React', 'Redux'],
-    image: 'img/projects/countries-world.png',
+    image: 'img/projects/countries-screen.png',
     description:
       'This React capstone project is about building a mobile web application to check a list of metrics using React and Redux. I selected an API that provides numeric data about countries then I built the webapp around it. The webapp has several pages: \n \
 - Home page with a list of countries that could be filtered by some parameters(Region, population...) and a random country at the top. \n- The detail page for the country details; in the screenshot, the detail page for the Cook Islands country with its information(Capital, Population, Currency, Region...).',
@@ -54,7 +54,7 @@ const projects = [
   {
     title: 'Budget app',
     technologies: ['Ruby', 'Ruby on rails', 'SASS','BOOTSTRAP'],
-    image: 'img/projects/budget.png',
+    image: 'img/projects/budget-screen.png',
     description:
       'The Budget app is a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
     liveLink: 'https://medamine-budget-app.herokuapp.com/',
