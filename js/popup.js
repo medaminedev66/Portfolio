032d3986e54b6d46d1b45acbeb03b3e49608605a
Amine Smahi <md.amine.smahi@gmail.com>
2022-02-12 15:12:47 +0100

    Add some new screenshots

--- js/popup.js
+++ js/popup.js
@@ -31,7 +31,7 @@ const projects = [
   {
     title: 'Countries of all the World',
     technologies: ['Css', 'Bootstrap', 'React', 'Redux'],
-    image: 'img/projects/countries-screen.png',
+    image: 'img/projects/countries-screen-2.png',
     description:
       'This React capstone project is about building a mobile web application to check a list of metrics using React and Redux. I selected an API that provides numeric data about countries then I built the webapp around it. The webapp has several pages: \n \
 - Home page with a list of countries that could be filtered by some parameters(Region, population...) and a random country at the top. \n- The detail page for the country details; in the screenshot, the detail page for the Cook Islands country with its information(Capital, Population, Currency, Region...).',
